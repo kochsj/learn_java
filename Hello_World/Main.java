26f6a0b4bc463e31f532b5b481dd542f5c64810d
Stephen Koch <kochsj@gmail.com>
2020-05-13 13:43:44 -0700

    init

---
+++ Hello_World/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // casting (float)4.5 OR 4.5f
+        float flt = 4.5f;
+        
+        System.out.println("This will be printed");
+    }
+}
